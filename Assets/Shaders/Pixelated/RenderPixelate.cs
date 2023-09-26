fb2b4bf55a64d0482d519ba8b83f0ba5f6200e6d
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-09-25 21:48:22 -0700

    pixels!

--- Assets/Shaders/Pixelated/RenderPixelate.cs
+++ Assets/Shaders/Pixelated/RenderPixelate.cs
@@ -1,52 +1,53 @@
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
 
-public class RenderPixelate : CustomPassVolume {
-	public ComputeShader PixelateComputeShader;
-	[Range(2, 40)] public int BlockSize = 3;
-
-	int _screenWidth;
-	int _screenHeight;
-	RTHandle _renderTexture;
-	Material _pixelateMaterial;
+class FancyPass : CustomPass {
+	private Shader fullscreenShader;
+	private Material fullscreenMaterial;
+	private Shader prePassShader;
+	private Material prePassMaterial;
+	private RTHandle intermediateRenderTarget;
+	private LayerMask pixelationLayer = ~0;
 
 	protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
-		CreateRenderTexture();
-		_pixelateMaterial = new Material(Shader.Find("RenderPixelate")); // Replace with your pixelate shader
+		// Create pre pass shader/material
+		prePassShader = Shader.Find("Custom/PrePass");
+		prePassMaterial = CoreUtils.CreateEngineMaterial(prePassShader);
+		Debug.Assert(fullscreenMaterial != null, "Failed to create fullscreen pass material");
 
-		// Add any setup code for your custom pass here.
+		// Create fullscreen shader/material
+		fullscreenShader = Shader.Find("Custom/FancyPass");
+		fullscreenMaterial = CoreUtils.CreateEngineMaterial(fullscreenShader);
+		Debug.Assert(fullscreenMaterial != null, "Failed to create fullscreen pass material");
+		fullscreenMaterial.SetColor(0, Color.red);
+
+		pixelationLayer = LayerMask.GetMask("Pixelate");
+
+		// Create render target for pre-pass
+		intermediateRenderTarget = RTHandles.Alloc(
+				Vector2.one, TextureXR.slices, dimension: TextureXR.dimension,
+				colorFormat: GraphicsFormat.B10G11R11_UFloatPack32,
+				useDynamicScale: true, name: "Immediate Test Buffer"
+		);
 	}
 
-	void CreateRenderTexture() {
-		_screenWidth = Screen.width;
-		_screenHeight = Screen.height;
+	protected override void Execute(CustomPassContext ctx) {
+		CustomPassUtils.DrawRenderers(ctx, pixelationLayer, RenderQueueType.All);
+		// Pre pass
+		CoreUtils.SetRenderTarget(ctx.cmd, intermediateRenderTarget, ctx.cameraDepthBuffer, ClearFlag.Color);
+		CustomPassUtils.DrawRenderers(ctx, pixelationLayer, RenderQueueType.All, prePassMaterial);
 
-		_renderTexture = RTHandles.Alloc(_screenWidth, _screenHeight, colorFormat: GraphicsFormat.R8G8B8A8_SRGB, filterMode: FilterMode.Point, enableRandomWrite: true);
-
-		// Set up the render texture parameters as needed.
+		// Fullscreen pass
+		ctx.propertyBlock.SetTexture("_InputTexture", intermediateRenderTarget);
+		CoreUtils.SetRenderTarget(ctx.cmd, ctx.cameraColorBuffer, ClearFlag.None);
+		CoreUtils.DrawFullScreen(ctx.cmd, fullscreenMaterial, ctx.propertyBlock, shaderPassId: 0);
 	}
 
-	protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera camera, CullingResults cullingResult) {
-		// Dispatch your compute shader here.
-		var mainKernel = PixelateComputeShader.FindKernel("Pixelate");
-		cmd.SetComputeIntParam(PixelateComputeShader, "_BlockSize", BlockSize);
-		cmd.SetComputeIntParam(PixelateComputeShader, "_ResultWidth", _renderTexture.rt.width);
-		cmd.SetComputeIntParam(PixelateComputeShader, "_ResultHeight", _renderTexture.rt.height);
-		cmd.SetComputeTextureParam(PixelateComputeShader, mainKernel, "_Result", _renderTexture);
-
-		uint threadGroupsX, threadGroupsY, threadGroupsZ;
-		PixelateComputeShader.GetKernelThreadGroupSizes(mainKernel, out threadGroupsX, out threadGroupsY, out threadGroupsZ);
-
-		cmd.DispatchCompute(PixelateComputeShader, mainKernel,
-				Mathf.CeilToInt(_renderTexture.rt.width / (float)BlockSize / threadGroupsX),
-				Mathf.CeilToInt(_renderTexture.rt.height / (float)BlockSize / threadGroupsY),
-				1);
-
-		// Blit the result to the destination.
-		HDUtils.DrawFullScreen(cmd, _renderTexture, destination: camera.colorBuffer, material: _pixelateMaterial);
-
-		// If you want to apply additional post-processing, you can do so here.
+	protected override void Cleanup() {
+		CoreUtils.Destroy(prePassMaterial);
+		CoreUtils.Destroy(fullscreenMaterial);
+		intermediateRenderTarget.Release();
 	}
 }
